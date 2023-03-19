a15b937b4a4029d9b2722cb0bc8b216ec0664517
Sebastian Pape <s@pape.io>
2023-03-19 15:34:33 +0100

    v1.20.9

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ if(_global$1.XMLHttpRequest == undefined) { _global$1.XMLHttpRequest = class XML
 if(_global$1.location == undefined) { _global$1.location = {} }
 if(_global$1.crypto == undefined) { _global$1.crypto = {} }
 if(_global$1.crypto.getRandomValues == undefined) { _global$1.crypto.getRandomValues = function(abv) { var l = abv.length; while (l--) { abv[l] = parseInt(Math.random().toString().replace('0.', ''), 10) }; return abv } }
-if(_global$1.fetch == undefined) { _global$1.fetch = async ()=>{}; _global$1._polyfillFetch = true }
+if(_global$1.fetch == undefined) { _global$1.fetch = async ()=>{ return _global$1.fetch(null, arguments) }; _global$1._polyfillFetch = true }
       `
     },
     {
@@ -39,7 +39,7 @@ if(_global$1.XMLHttpRequest == undefined) { _global$1.XMLHttpRequest = class XML
 if(_global$1.location == undefined) { _global$1.location = {} }
 if(_global$1.crypto == undefined) { _global$1.crypto = {} }
 if(_global$1.crypto.getRandomValues == undefined) { _global$1.crypto.getRandomValues = function(abv) { var l = abv.length; while (l--) { abv[l] = parseInt(Math.random().toString().replace('0.', ''), 10) }; return abv } }
-if(_global$1.fetch == undefined) { _global$1.fetch = async ()=>{}; _global$1._polyfillFetch = true }
+if(_global$1.fetch == undefined) { _global$1.fetch = async ()=>{ return _global$1.fetch(null, arguments) }; _global$1._polyfillFetch = true }
       `
     },
   ],
